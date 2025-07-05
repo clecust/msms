75d1c15769f53b0b8ff6b13e3641bbe55b03555b
junbao_iccas <1826680713@qq.com>
2025-07-05 21:06:35 +0800

    避免保存h5文件

--- process/batch_eval.sh
+++ process/batch_eval.sh
@@ -14,7 +14,7 @@ conda activate py310pt250cu124
 # --format libtorch mliap
 
 # # # 处理所有匹配的文件 ecemc
-for model_file in /home/giga/BIG/data/ML_TrainTest_ECEMC/ecemc/al/work_ecemc_b3lyp_4090_c8352Y_v5/data/mace_ecemcv5d15virial_cut800_64l2r6n2_b32_e0_swa_stress1001000_run-7050*_stagetwo.model ; do
+for model_file in /home/giga/BIG/data/ML_TrainTest_ECEMC/ecemc/al/work_ecemc_b3lyp_4090_c8352Y_v5/data/mace_npjTrain16_PBED3w101_64l2r6n2_b32_ea_swa_stress1001000_run-7050*_stagetwo.model ; do
 # for model_file in /home/giga/BIG/data/ML_TrainTest_ECEMC/TrainingData/al/work_ecemc_pbe_4090_c8352Y_v42/data/mace_pbev42d14virial_cut800_64l2r6n2_b32_e0_swa_stress1001000_run-7040*_stagetwo.model ; do
     echo "处理文件: $model_file"
     python3 /home/giga/code/msms/mace/cli/create_lammps_model.py \
